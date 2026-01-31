50745703efbcf2b4c5cc358fae3b6ff8ffda7d57
GitHub Action <action@github.com>
2026-01-31 11:19:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/05nuha/devcare/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/05nuha/devcare/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.05nuha.devcare.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
